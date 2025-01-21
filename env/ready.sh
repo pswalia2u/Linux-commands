71455b24f3313fc4207436c179de52f205041260
Prabhsimran <pswalia2u@gmail.com>
2025-01-21 10:59:39 +0000

    Update ready.sh

--- env/ready.sh
+++ env/ready.sh
@@ -13,3 +13,4 @@ sudo apt-get install sublime-text git -y
 sudo wget -qO- https://api.github.com/repos/VSCodium/vscodium/releases/latest | grep browser_download_url | grep amd64.deb | cut -d '"' -f 4 | wget -i - && sudo apt install -y ./codium*.deb && rm codium*.deb
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh-autosuggestions
 sudo apt install -y openssh-server && sudo systemctl start ssh && sudo systemctl enable
+[ -d "/home/linuxbrew/.linuxbrew/bin" ] && echo 'export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin' >> ~/.zshrc && source ~/.zshrc
